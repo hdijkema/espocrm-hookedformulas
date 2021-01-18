5878c7dceac3c1251548028eb5c71a7b5f4af3cf
Hans Dijkema <hans@dykema.nl>
2021-01-18 14:44:35 +0100

    Oeps, glitch

--- application/Espo/Modules/HookedFormulas/Core/Formula/Functions/ToIntType.php
+++ application/Espo/Modules/HookedFormulas/Core/Formula/Functions/ToIntType.php
@@ -43,7 +43,7 @@ class ToIntType extends \Espo\Core\Formula\Functions\AttributeType
             throw new Error();
         }
 
-        if (count($item->value) == 1 ) {
+        if (count($item->value) != 1 ) {
             throw new Error();
         }
 
